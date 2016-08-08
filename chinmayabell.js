bbafe97591dd1cec9919a0c60796e2063bd72974
Srijay Kasturi (TechFilmer) <srijay@techfilmer.com>
2016-08-08 18:05:07 -0400

    uploading new files

---
+++ chinmayabell.js
@@ -0,0 +1,27 @@
+function runOnTimes(cb, t) {
+    (function loop() {
+ 
+        if (t(10, 10) === true || t(11, 10) === true || t(13, 0) === true || t(14, 0) === true || t(15, 5) === true || t(11, 0) === true || t(12, 0) === true || t(13, 45) === true || t(14, 45) === true || t(15, 50) === true || t(18, 4) === true) {
+            cb(); 
+        }
+        now = new Date();                  // allow for time passing
+        var delay = 60000 - (now % 60000); // exact ms to next minute interval
+        setTimeout(loop, delay);
+    })();
+}
+
+function getTime(h, m) {
+	var d = new Date();
+	if(d.getHours() === h && d.getMinutes() === m){
+		return true;
+	}
+	return false;
+}
+
+function playBell() {
+	console.log("playing music..."); 
+	var audio = new Audio('bell.mp3');
+	audio.play();
+}
+
+runOnTimes(playBell, getTime);
