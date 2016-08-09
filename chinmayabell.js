ce990adec4fa9132448f5112409cfff3e5010faa
Srijay Kasturi (TechFilmer) <srijay@techfilmer.com>
2016-08-09 10:03:09 -0400

    Update chinmayabell.js

--- chinmayabell.js
+++ chinmayabell.js
@@ -1,4 +1,5 @@
 function runOnTimes(cb, t) {
+	document.getElementById("dank").innerHTML = "Program Started!";
     (function loop() {
  
         if (t(10, 10) === true || t(11, 10) === true || t(13, 0) === true || t(14, 0) === true || t(15, 5) === true || t(11, 0) === true || t(12, 0) === true || t(13, 45) === true || t(14, 45) === true || t(15, 50) === true || t(8, 30) === true) {
