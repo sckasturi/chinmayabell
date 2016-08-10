050a9f0488c4d4a5d38834201c83dbb22138c30e
Srijay Kasturi (TechFilmer) <srijay@techfilmer.com>
2016-08-10 16:31:34 -0400

    Update chinmayabell.js

--- chinmayabell.js
+++ chinmayabell.js
@@ -2,7 +2,7 @@ function runOnTimes(cb, t) {
 	document.getElementById("dank").innerHTML = "Program started!";
     (function loop() {
  
-        if (t(10, 10) === true || t(11, 10) === true || t(13, 0) === true || t(14, 0) === true || t(15, 5) === true || t(11, 0) === true || t(12, 0) === true || t(13, 45) === true || t(14, 45) === true || t(15, 50) === true || t(8, 30) === true) {
+        if (t(10, 0) || t(10, 10) === true || t(11, 10) === true || t(13, 0) === true || t(14, 0) === true || t(15, 5) === true || t(11, 0) === true || t(12, 0) === true || t(13, 45) === true || t(14, 45) === true || t(15, 50) === true || t(15, 0) === true || t(12, 50) === true ){
             cb(); 
         }
         now = new Date();                  // allow for time passing
